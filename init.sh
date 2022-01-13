f088ec5954bb28d63668011591bf6c2f47779c91
Etienne Cochard <etienne.cochard@gmail.com>
2022-01-13 14:44:24 +0100

    removed x4

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# git submodule add https://github.com/eco747/x4.git
-
+git submodule add -b main https://github.com/eco747/x4.git
 mkdir dist -p
 cp ./assets/index.html ./dist
 cp -r ./assets/css ./dist
