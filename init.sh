6a221cc20a5ac8097f9f24c48ed286fce6ac144a
Etienne Cochard <etienne.cochard@gmail.com>
2022-01-13 14:32:49 +0100

    init script

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-git submodule add https://github.com/eco747/x4.git
+# git submodule add https://github.com/eco747/x4.git
 
-mkdir dist
-mkdir src
-
+mkdir dist -p
 cp ./assets/index.html ./dist
-cp ./assets/*.less src
 cp -r ./assets/css ./dist
 cp ./assets/css/favicon.ico ./dist
