625e0dfd7d84300e8c78ff71f82a256632d33827
Etienne <etienne.cochard@gmail.com>
2022-02-18 14:13:55 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { Application, VLayout, Label, html } from './x4deps.js';
 class MainFrame extends VLayout {
 	render( ) {
 		this.setContent( [
-			new Label( { tag: 'h1', text: "<h1>Hello from X4</h1>" } );
-		])
+			new Label( { tag: 'h1', text: "Hello from X4" } );
+		]);
 	}
 }
 
